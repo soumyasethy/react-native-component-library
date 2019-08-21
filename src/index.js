3eb41065251528545c301ddd709100c11f3c83ad
Soumya Ranjan Sethy <soumyar@licious.com>
2019-08-21 18:02:56 +0530

    Clean

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { View, Text } from "react-native";
+// import HeaderWithShareIcon from "./header";
+// export { HeaderWithShareIcon };
 
 export default class ReactNativeLibraryStarter extends React.Component {
   render() {
     return (
       <View>
-        <Text>This is library starter. Enjoy. Some more Text.....</Text>
+        <Text> Licious Vivek</Text>
+        <Text>
+          Hello Soumya Kiran!!! This is library starter. Enjoy. Some more
+          Text.....
+        </Text>
       </View>
     );
   }
